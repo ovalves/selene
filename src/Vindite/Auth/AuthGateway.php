b599ebdbb10aa1482aa3de6f30e95c715e88b70a
Vinicius Alves <vinicius_o.a@live.com>
2019-10-14 17:16:10 -0300

    Gerenciamento de login de usser, gerenciamento dos dados do password com a libsodium

---
+++ src/Vindite/Auth/AuthGateway.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @copyright   2019 - Vindite
+ * @author      Vinicius Oliveira <vinicius_o.a@live.com>
+ * @category    Micro Framework
+ * @since       2019-10-14
+ */
+
+namespace Vindite\Auth;
+
+use Vindite\Gateway\GatewayAbstract;
+use Vindite\Config\ConfigConstant;
+
+/**
+ * Manages the authentication data
+ */
+class AuthGateway extends GatewayAbstract
+{
+    use \Vindite\Config\ConfigAwareTrait;
+
+    /**
+     * Find user by email
+     *
+     * @param string $email
+     * @return array
+     */
+    public function findByEmail(string $email) : array
+    {
+        $config = $this->loadConfig(ConfigConstant::AUTH);
+
+        if (empty($config[ConfigConstant::AUTH_TABLE_NAME])) {
+            throw new \Exception("Failed to check user auth data");
+        }
+
+        return $this
+                ->select('*')
+                ->table($config[ConfigConstant::AUTH_TABLE_NAME])
+                ->where(['email = ?' => $email])
+                ->execute()
+                ->fetchAll();
+    }
+
+    /**
+     * Register user
+     *
+     * @param string $email
+     * @param string $password
+     * @return bool
+     */
+    public function registerUser(string $email, string $password) : bool
+    {
+        return (bool) $this->insert([
+            'email' => $email,
+            'password' => $password,
+        ])
+        ->table('user')
+        ->execute();
+    }
+}
