0ebe4c85de9bdc87e0d685fdc19606738ec1e1b8
ovalves <vinicius_o.a@live.com>
2021-06-23 12:58:42 -0300

    Fix: Refatoção na trait de routing e criação de metodo que faz o resolver de recursos com query params

--- src/Selene/Routes/RouteAwareTrait.php
+++ src/Selene/Routes/RouteAwareTrait.php
@@ -8,88 +8,84 @@
 
 namespace Selene\Routes;
 
-use Selene\Routes\RouteException;
-
 trait RouteAwareTrait
 {
     protected $matchUri;
     protected $matchParam;
+
     /**
-     * Indica o recurso não encontrado
-     *
-     * @return string
+     * Indica o recurso não encontrado.
      */
-    public function resourceNotFound($resource = null) : string
+    public function resourceNotFound($resource = null): string
     {
         if (empty($resource)) {
-            return "Recurso não encontrado";
+            return 'Recurso não encontrado';
         }
 
-        return \sprintf("Recurso (%s) não encontrado", $resource);
+        return \sprintf('Recurso (%s) não encontrado', $resource);
     }
 
     /**
-     * Indica que o método http não é permitido
-     *
-     * @return string
+     * Indica que o método http não é permitido.
      */
-    public function controllerActionNotFound($controller = null, $action = null) : string
+    public function controllerActionNotFound($controller = null, $action = null): string
     {
         if (\is_object($controller)) {
             $controller = get_class($controller);
         }
 
         if (empty($action)) {
-            return "Action da controller não especificada";
+            return 'Action da controller não especificada';
         }
 
         if (empty($controller)) {
-            return "Controller não especificada";
+            return 'Controller não especificada';
         }
 
-        return \sprintf("Action (%s) da controller (%s) não encontrada", $action, $controller);
+        return \sprintf('Action (%s) da controller (%s) não encontrada', $action, $controller);
     }
 
     /**
-     * Indica que o método http não é permitido
-     *
-     * @return string
+     * Indica que o método http não é permitido.
      */
-    public function methodHttpNotFound($httpMethod = null) : string
+    public function methodHttpNotFound($httpMethod = null): string
     {
         if (empty($httpMethod)) {
-            return "Método http não encontrado";
+            return 'Método http não encontrado';
         }
 
-        return \sprintf("Método http (%s) não encontrado", $httpMethod);
+        return \sprintf('Método http (%s) não encontrado', $httpMethod);
     }
 
     /**
-     * Resolve o recurso requisitado e seus argumentos
-     *
-     * @param string $routeResource
-     * @param string $calledResource
-     * @return bool
+     * Resolve o recurso requisitado.
      */
-    public function resolveResourceArgument(string $routeResource, string $calledResource) : bool
+    public function resolveResource(string $routeResource, string $calledResource): bool
     {
         if (empty($routeResource)) {
-            throw new RouteException("O recurso solicitado não existe", 404);
+            throw new RouteException('O recurso solicitado não existe', 404);
         }
 
         if (empty($calledResource)) {
-            throw new RouteException("O recurso solicitado não existe", 404);
+            throw new RouteException('O recurso solicitado não existe', 404);
         }
 
-        preg_match_all('/(\{\w+\})/', $routeResource, $matches);
+        \preg_match_all('/(\{\w+\})/', $routeResource, $args);
 
-        if (empty($matches[0])) {
-            return $routeResource === $calledResource;
+        if (!empty($args[0])) {
+            return $this->resolveResourceWithArgument($routeResource, $calledResource, $args);
         }
 
-        $routeResource = explode('/', $routeResource);
-        $calledResource = explode('/', $calledResource);
+        return $this->resolveResourceWithQueryParams($routeResource, $calledResource);
+    }
 
+    /**
+     * Resolve o recurso requisitado e seus argumentos.
+     */
+    protected function resolveResourceWithArgument(string $routeResource, string $calledResource, array $args): bool
+    {
+        $routeResource = \explode('/', $routeResource);
+        $calledResource = \explode('/', $calledResource);
 
         $this->matchUri = [];
         $this->matchParam = [];
@@ -98,23 +94,47 @@ trait RouteAwareTrait
                 continue;
             }
 
-            $resource = trim($resource);
+            $resource = \trim($resource);
             if (!isset($calledResource[$key])) {
                 continue;
             }
 
-            $calledResource[$key] = trim($calledResource[$key]);
+            $calledResource[$key] = \trim($calledResource[$key]);
             if ($resource === $calledResource[$key]) {
                 $this->matchUri = $resource;
                 continue;
             }
 
-            if (in_array($resource, $matches[0])) {
+            if (\in_array($resource, $args[0])) {
                 $this->matchParam = $calledResource[$key];
                 continue;
             }
         }
 
         return (empty($this->matchUri)) ? false : true;
+    }
+
+    /**
+     * Resolve o recurso requisitado e seus query params.
+     */
+    protected function resolveResourceWithQueryParams(string $routeResource, string $calledResource): string
+    {
+        \preg_match('/([^\?]+)(\?.*)?/', $calledResource, $args);
+
+        if (!empty($args[0])) {
+            $calledResource = \explode('?', $calledResource);
+        }
+
+        if (!empty($calledResource[1])) {
+            $params = \explode('&', $calledResource[1]);
+        }
+
+        $this->matchParam = [];
+        foreach ($params as $param) {
+            $params = \explode('=', $param);
+            $this->matchParam[$params[0]] = $params[1];
+        }
+
+        return $routeResource === $calledResource[0];
     }
 }
