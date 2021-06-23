cd3a1e4121a8748a33da1637d56f3698a5b2c053
ovalves <vinicius_o.a@live.com>
2021-06-23 17:45:33 -0300

    Feat: Adição de metodos que criam as clausulas de offset, limit e order by na montagem das querys

--- src/Selene/Database/Grammar/GrammarAbstract.php
+++ src/Selene/Database/Grammar/GrammarAbstract.php
@@ -8,79 +8,97 @@
 
 namespace Selene\Database\Grammar;
 
-use Selene\Database\Transaction;
-use Selene\Database\Builder\Where;
-use Selene\Database\Builder\Join;
-use Selene\Database\Builder\Group;
 use PDOStatement;
+use Selene\Database\Builder\Group;
+use Selene\Database\Builder\Join;
+use Selene\Database\Builder\Where;
 use Selene\Database\DatabaseException;
+use Selene\Database\Transaction;
 
 /**
- * Responsavel por disponibilizar os métodos comuns entre as classes de builder de query
+ * Responsavel por disponibilizar os métodos comuns entre as classes de builder de query.
  */
 abstract class GrammarAbstract
 {
     /**
-     * Guarda a transação ativa
+     * Guarda a transação ativa.
      *
      * @var Transaction
      */
     protected $transaction;
 
     /**
-     * Guarda os campos a serem usados na clausula
+     * Guarda os campos a serem usados na clausula.
      *
      * @var mixed
      */
     protected $fields;
 
     /**
-     * Guarda os valores a serem usados na clausula
+     * Guarda os valores a serem usados na clausula.
      *
      * @var mixed
      */
     protected $values;
 
     /**
-     * Guarda os valores a serem usados como bind na clausula
+     * Guarda os valores a serem usados como bind na clausula.
      *
      * @var mixed
      */
     protected $bindParam;
 
     /**
-     * Guarda a tabela a ser usada na clausula
+     * Guarda a tabela a ser usada na clausula.
      *
      * @var mixed
      */
     protected $table;
 
     /**
-     * Guarda os dados que serão usados na clausula where
+     * Guarda os dados que serão usados na clausula where.
      *
      * @var array
      */
     protected $where = [];
 
     /**
-     * Guarda os dados que serão usados na clausula join
+     * Guarda os dados que serão usados na clausula order.
+     *
+     * @var array
+     */
+    protected $order = [];
+
+    /**
+     * Guarda o valor que será usado no offset na montagem da query.
+     *
+     * @var int
+     */
+    protected $offset = 0;
+
+    /**
+     * Guarda o valor que será usado no limit na montagem da query.
+     *
+     * @var int
+     */
+    protected $limit = 15;
+
+    /**
+     * Guarda os dados que serão usados na clausula join.
      *
      * @var string
      */
     protected $join = '';
 
     /**
-     * Guarda os dados que serão usados na clausula group
+     * Guarda os dados que serão usados na clausula group.
      *
      * @var string
      */
     protected $group = '';
 
     /**
-     * Constructor
-     *
-     * @param Transaction $transaction
-     * @param mixed $fields
+     * Constructor.
      */
     final public function __construct(Transaction $transaction, $fields = null)
     {
@@ -91,98 +109,149 @@ abstract class GrammarAbstract
     }
 
     /**
-     * Cria uma claususa where
-     *
-     * @param mixed $data
-     * @return self
+     * Cria uma claususa where.
      */
-    final public function where(array $where) : self
+    final public function where(array $where): self
     {
         $this->where[] = $where;
+
         return $this;
     }
 
     /**
-     * Seta a tabela corrente
-     *
-     * @param string $table
-     * @return void
+     * Cria uma claususa order by.
      */
-    final public function table(string $table) : self
+    final public function order(string $column, string $sort = 'ASC'): self
+    {
+        $this->order[] = \addslashes($column) . ' ' . \addslashes($sort);
+
+        return $this;
+    }
+
+    /**
+     * Cria uma claususa offset.
+     */
+    final public function offset(int $offset): self
+    {
+        $this->offset = (int) ($offset < 1) ? 0 : $offset;
+
+        return $this;
+    }
+
+    /**
+     * Cria uma claususa limit.
+     */
+    final public function limit(int $limit): self
+    {
+        $this->limit = (int) ($limit < 1) ? 1 : $limit;
+
+        return $this;
+    }
+
+    /**
+     * Seta a tabela corrente.
+     */
+    final public function table(string $table): self
     {
         $this->table = \addslashes($table);
+
         return $this;
     }
 
     /**
-     * Retorna os dados da clausula where
-     *
-     * @return Where
+     * Retorna os dados da clausula where.
      */
-    final protected function getWhere() : Where
+    final protected function getWhere(): Where
     {
         return new Where($this->where);
     }
 
     /**
-     * Retorna os dados da clausula join
-     *
-     * @return Join
+     * Retorna os dados da clausula order.
      */
-    final protected function getJoin() : Join
+    final protected function getOrder(): string
+    {
+        $orderString = 'ORDER BY ';
+        $orderCount = count($this->order) - 1;
+        for ($i = 0; $i <= $orderCount; $i++) {
+            if ($i >= $orderCount) {
+                $orderString .= "{$this->order[$i]}";
+                continue;
+            }
+
+            $orderString .= "{$this->order[$i]}, ";
+        }
+
+        return $orderString;
+    }
+
+    /**
+     * Retorna o valor do limit que será usado na query.
+     */
+    final protected function getLimit(): string
+    {
+        return "LIMIT {$this->limit}";
+    }
+
+    /**
+     * Retorna o valor do offset que será usado na query.
+     */
+    final protected function getOffset(): string
+    {
+        return "OFFSET {$this->offset}";
+    }
+
+    /**
+     * Retorna os dados da clausula join.
+     */
+    final protected function getJoin(): Join
     {
         return new Join($this->join);
     }
 
     /**
-     * Retorna os dados da clausula group
-     *
-     * @return Group
+     * Retorna os dados da clausula group.
      */
-    final protected function getGroup() : Group
+    final protected function getGroup(): Group
     {
         return new Group($this->group);
     }
 
     /**
-     * Checa se os campos estão formatados corretamente
-     *
-     * @return bool
+     * Checa se os campos estão formatados corretamente.
      */
-    final public function checkFields() : bool
+    final public function checkFields(): bool
     {
         if (empty($this->fields)) {
-            throw new DatabaseException("Erro ao processar a query os campos não podem ser vazios");
+            throw new DatabaseException('Erro ao processar a query os campos não podem ser vazios');
         }
 
         if (\is_array($this->fields)) {
             $this->prepare($this->fields);
+
             return true;
         }
 
         $this->fields = $this->escape($this->fields);
+
         return true;
     }
 
     /**
-     * Checa se a tabela esta preenchida corretamente
-     *
-     * @return void
+     * Checa se a tabela esta preenchida corretamente.
      */
-    final public function checkTable() : void
+    final public function checkTable(): void
     {
         if (empty($this->table)) {
-            throw new DatabaseException("Tabela não encontrada", 500);
+            throw new DatabaseException('Tabela não encontrada', 500);
         }
     }
 
     /**
      * Itera pelos elemantos do statement
-     * Executa o escape da query
-     * @param arary $data
-     * @return void
+     * Executa o escape da query.
      */
-    protected function prepare(array $data) : void
+    protected function prepare(array $data): void
     {
         $prepared = [];
         foreach ($data as $key => $value) {
@@ -197,15 +266,14 @@ abstract class GrammarAbstract
     }
 
     /**
-     * Executa o escape da query
+     * Executa o escape da query.
      *
      * @param mixed $value
-     * @return mixed
      */
-    protected function escape($value)
+    protected function escape($value): mixed
     {
         if (!\is_scalar($value)) {
-            return "null";
+            return 'null';
         }
 
         if (\is_string($value) and (!empty($value))) {
@@ -213,21 +281,20 @@ abstract class GrammarAbstract
         }
 
         if (\is_bool($value)) {
-            return $value ? 'true': 'false';
+            return $value ? 'true' : 'false';
         }
 
-        if ($value !== '') {
+        if ('' !== $value) {
             return $value;
         }
 
-        return "null";
+        return 'null';
     }
 
     /**
-     * Executa a query
+     * Executa a query.
      *
      * @param array $whereClause
-     * @return PDOStatement
      */
-    abstract public function execute() : PDOStatement;
+    abstract public function execute(): PDOStatement;
 }
