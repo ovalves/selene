8770a0c8561920851afceb3e703a1ca314a7a95d
ovalves <vinicius_o.a@live.com>
2021-06-23 17:46:00 -0300

    Feat: Adição das strings de grammar que definem as clausulas de offset, limit e order by na montagem das querys

--- src/Selene/Database/Grammar/GrammarAwareTrait.php
+++ src/Selene/Database/Grammar/GrammarAwareTrait.php
@@ -11,18 +11,18 @@ namespace Selene\Database\Grammar;
 use Selene\Database\DatabaseConstant;
 
 /**
- * Responsavel por executar os diferentes tipos de statement
+ * Responsavel por executar os diferentes tipos de statement.
  */
 trait GrammarAwareTrait
 {
     /**
-     * Formas de executar as clausulas
+     * Formas de executar as clausulas.
      *
      * @var array
      */
     protected $grammar = [
         DatabaseConstant::INSERT => 'INSERT INTO __TABLENAME__ ( __FIELDS__ ) VALUES ( __VALUES__ )',
-        DatabaseConstant::SELECT => 'SELECT __FIELDS__ FROM __TABLENAME__ __JOIN__ __WHERE__ __GROUP__',
+        DatabaseConstant::SELECT => 'SELECT __FIELDS__ FROM __TABLENAME__ __JOIN__ __WHERE__ __ORDER__ __LIMIT__ __OFFSET__ __GROUP__',
         DatabaseConstant::UPDATE => 'UPDATE __TABLENAME__ __JOIN__ SET __FIELDS__ __WHERE__',
         DatabaseConstant::DELETE => 'DELETE FROM __TABLENAME__ __WHERE__',
     ];
